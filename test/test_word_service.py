f18deefff6fe48fe941a25777879d267c5fdf0f2
Scandinaf_ <sergeyqwertyborovskiy@gmail.com>
2018-12-20 20:04:46 +0300

    Were added tests for the service method update_word

--- test/test_word_service.py
+++ test/test_word_service.py
@@ -20,6 +20,39 @@ __mock_objects__()
 
 
 @pytest.mark.asyncio
+async def test_update_word_correct():
+    word_service.db_layer.word.record_is_exists.return_value = __default_coroutine__(True)
+    word_service.db_layer.word.update.return_value = __default_coroutine__()
+    result = await word_service.update_word("""{"word" : "bad", "translation": "плохой"}""")
+    assert result == "Word was updated!!!"
+
+
+@pytest.mark.asyncio
+async def test_update_word_invalid_json():
+    result = await word_service.update_word("""{"translation": "плохой"}""")
+    assert isinstance(result, Error)
+
+    result = await word_service.update_word("""{"word_new": "bad"}""")
+    assert isinstance(result, Error)
+
+    result = await word_service.update_word([])
+    assert isinstance(result, Error)
+
+    result = await word_service.update_word(123)
+    assert isinstance(result, Error)
+
+    result = await word_service.update_word("just text")
+    assert isinstance(result, Error)
+
+
+@pytest.mark.asyncio
+async def test_update_word_not_found():
+    word_service.db_layer.word.record_is_exists.return_value = __default_coroutine__(False)
+    result = await word_service.update_word("""{"word" : "bad", "translation": "плохой"}""")
+    assert isinstance(result, Error)
+
+
+@pytest.mark.asyncio
 async def test_get_word_not_found():
     word_service.db_layer.word.find_one_by_word.return_value = __default_coroutine__()
     result = await word_service.get_word("test")
@@ -61,7 +94,7 @@ async def test_not_valid_json():
     result = await word_service.save_word("{}")
     assert isinstance(result, Error)
 
-    result = await word_service.save_word("""{"word" : "Anyone who reads Old and Middle English literary texts"}""")
+    result = await word_service.save_word("""{"translation" : "Anyone who reads Old and Middle English literary texts"}""")
     assert isinstance(result, Error)
 
     result = await word_service.save_word("""{"word" : 123}""")
